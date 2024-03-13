f5826535674fb330d0c394c69579771a02ae38dc
dahlo <m.dahlo@gmail.com>
2024-03-14 00:21:30 +0100

    Added more functions

--- bjornlunden_document_downloader.py
+++ bjornlunden_document_downloader.py
@@ -3,18 +3,22 @@ import requests
 import yaml
 import time
 import os
+from pprint import pprint
+import pdb
 
 TOKEN_FILE = 'token_cache.txt'
+
 
 def fetch_api_token(base_url_auth, client_id, client_secret):
     payload = {
         'grant_type': 'client_credentials',
-        'scope': '',
+#        'scope': '',
         'client_id': client_id,
         'client_secret': client_secret
     }
 
     response = requests.post(f'{base_url_auth}/token', data=payload)
+    print(response.json())
 
     if response.status_code == 200:
         api_token = response.json().get('access_token')
@@ -23,7 +27,7 @@ def fetch_api_token(base_url_auth, client_id, client_secret):
         with open(TOKEN_FILE, 'w') as file:
             file.write(f'{api_token}\n{expiration_time}')
 
-        os.chmod(TOKEN_FILE, 0o400)  # Set file permissions to allow only the user to read
+        os.chmod(TOKEN_FILE, 0o600)  # Set file permissions to allow only the user to read
 
         print(f'API Token: {api_token}')
         return api_token
@@ -31,13 +35,14 @@ def fetch_api_token(base_url_auth, client_id, client_secret):
         print(f'Failed to fetch API token. Status code: {response.status_code}')
         return None
 
+
 def list_connected_companies(base_url, api_token):
     headers = {
         'Content-Type': 'application/json',
         'Authorization': f'Bearer {api_token}'
     }
 
-    response = requests.get(f'{base_url}/meta/allKeys', headers=headers)
+    response = requests.get(f'{base_url}/common/client', headers=headers)
 
     if response.status_code == 200:
         connected_companies = response.json()
@@ -46,6 +51,25 @@ def list_connected_companies(base_url, api_token):
             print(company)
     else:
         print(f'Failed to list connected companies. Status code: {response.status_code}')
+
+
+def get_company_details(base_url, api_token, user_key):
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Key': user_key,
+        'Authorization': f'Bearer {api_token}'
+    }
+
+    response = requests.get(f'{base_url}/details', headers=headers)
+
+    if response.status_code == 200:
+        company_details = response.json()
+        print('Company Details:')
+        print(company_details)
+    else:
+        print(f'Failed to get company details. Status code: {response.status_code}')
+        pdb.set_trace()
+
 
 def get_cached_token():
     try:
@@ -73,8 +97,10 @@ if __name__ == '__main__':
     client_id = config.get('client_id')
     client_secret = config.get('client_secret')
     base_url = config.get('base_url')
+    user_key = config.get('user_key')
 
-    if not all([base_url_auth, client_id, client_secret, base_url]):
+
+    if not all([base_url_auth, client_id, client_secret, base_url, user_key]):
         print('Missing required information in the config file.')
     else:
         cached_token = get_cached_token()
@@ -86,4 +112,5 @@ if __name__ == '__main__':
 
         if cached_token:
             list_connected_companies(base_url, cached_token)
+            get_company_details(base_url, cached_token, user_key)
 
