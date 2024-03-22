173db4605f73003a3c025d8d44f1b430701c03a0
dahlo <m.dahlo@gmail.com>
2024-03-22 08:49:59 +0100

    Added downloading of invoices.

--- bjornlunden_document_downloader.py
+++ bjornlunden_document_downloader.py
@@ -29,14 +29,13 @@ def fetch_api_token(base_url_auth, client_id, client_secret):
 
         os.chmod(TOKEN_FILE, 0o600)  # Set file permissions to allow only the user to read
 
-        print(f'API Token: {api_token}')
         return api_token
     else:
         print(f'Failed to fetch API token. Status code: {response.status_code}')
         return None
 
 
-def list_connected_companies(base_url, api_token):
+def get_connected_companies(base_url, api_token):
     headers = {
         'Content-Type': 'application/json',
         'Authorization': f'Bearer {api_token}'
@@ -46,9 +45,7 @@ def list_connected_companies(base_url, api_token):
 
     if response.status_code == 200:
         connected_companies = response.json()
-        print('Connected Companies:')
-        for company in connected_companies:
-            print(company)
+        return connected_companies
     else:
         print(f'Failed to list connected companies. Status code: {response.status_code}')
 
@@ -64,11 +61,108 @@ def get_company_details(base_url, api_token, user_key):
 
     if response.status_code == 200:
         company_details = response.json()
-        print('Company Details:')
-        print(company_details)
+        return company_details
     else:
         print(f'Failed to get company details. Status code: {response.status_code}')
-        pdb.set_trace()
+
+
+def get_documents(base_url, api_token, user_key):
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Key': user_key,
+        'Authorization': f'Bearer {api_token}'
+    }
+
+    response = requests.get(f'{base_url}/document', headers=headers)
+
+    if response.status_code == 200:
+        documents = response.json()
+        return documents
+    else:
+        print(f'Failed to get documents. Status code: {response.status_code}')
+
+
+def get_document_metadata(base_url, api_token, user_key, id):
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Key': user_key,
+        'Authorization': f'Bearer {api_token}'
+    }
+
+    response = requests.get(f'{base_url}/document/{id}/meta', headers=headers)
+
+    if response.status_code == 200:
+        document = response.json()
+        return document
+    else:
+        print(f'Failed to get document "{id}". Status code: {response.status_code}')
+
+
+def get_accounts(base_url, api_token, user_key):
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Key': user_key,
+        'Authorization': f'Bearer {api_token}'
+    }
+
+    response = requests.get(f'{base_url}/account', headers=headers)
+
+    if response.status_code == 200:
+        accounts_raw = response.json()
+        accounts = { account['id']: account for account in accounts_raw }
+        return accounts
+    else:
+        print(f'Failed to get document "{id}". Status code: {response.status_code}')
+
+
+def fetch_journal_entries(base_url, api_token, user_key, startdate, enddate, rows=1000):
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Key': user_key,
+        'Authorization': f'Bearer {api_token}'
+    }
+
+    response = requests.get(f'{base_url}/journal/entry/batch?startdate={startdate}&enddate={enddate}&rows={rows}', headers=headers)
+
+    if response.status_code == 200:
+        journal_entries = response.json()['data']
+        return journal_entries
+    else:
+        print(f'Failed to get journal entries. Status code: {response.status_code}')
+
+
+def save_document_pdf(base_url, api_token, user_key, id, file_name):
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Key': user_key,
+        'Authorization': f'Bearer {api_token}'
+    }
+
+    response = requests.get(f'{base_url}/document/asPdf/{id}', headers=headers)
+
+    if response.status_code == 200:
+        with open(file_name, 'wb') as file:
+            file.write(response.content)
+
+    else:
+        print(f'Failed to get document "{id}". Status code: {response.status_code}')
+
+
+# broken, use save_document_pdf instead
+def get_document(base_url, api_token, user_key, id):
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Key': user_key,
+        'Authorization': f'Bearer {api_token}'
+    }
+
+    response = requests.get(f'{base_url}/document/{id}', headers=headers)
+
+    if response.status_code == 200:
+        document = response.json()
+        return document
+    else:
+        print(f'Failed to get document "{id}". Status code: {response.status_code}')
 
 
 def get_cached_token():
@@ -86,11 +180,12 @@ def get_cached_token():
 if __name__ == '__main__':
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Fetch API token using client ID and client secret from config YAML file')
-    parser.add_argument('config_file', help='Path to the config YAML file')
+    parser.add_argument('-c', '--config', help='Path to the config YAML file', required=True)
+    parser.add_argument('-o', '--output-dir', help='Path to the output directory', required=True)
     args = parser.parse_args()
 
     # Read the config file
-    with open(args.config_file, 'r') as file:
+    with open(args.config, 'r') as file:
         config = yaml.safe_load(file)
 
     base_url_auth = config.get('base_url_auth')
@@ -99,18 +194,41 @@ if __name__ == '__main__':
     base_url = config.get('base_url')
     user_key = config.get('user_key')
 
+    output = args.output_dir
 
     if not all([base_url_auth, client_id, client_secret, base_url, user_key]):
         print('Missing required information in the config file.')
     else:
         cached_token = get_cached_token()
 
-        if cached_token:
-            print(f'Using cached API Token: {cached_token}')
-        else:
+        if not cached_token:
             cached_token = fetch_api_token(base_url_auth, client_id, client_secret)
 
         if cached_token:
-            list_connected_companies(base_url, cached_token)
-            get_company_details(base_url, cached_token, user_key)
+            #pprint(get_connected_companies(base_url, cached_token))
+            #pprint(get_company_details(base_url, cached_token, user_key))
+
+            # get all account data
+            accounts = get_accounts(base_url, cached_token, user_key)
+
+            # get all journal entries
+            journal_entries = fetch_journal_entries(base_url, cached_token, user_key, '1920-01-01', '2023-12-31')
+
+            # find the ones with documents
+            journal_with_documents = [entry for entry in journal_entries if entry['documentIds']]
+            for journal in journal_with_documents:
+                for document_id in journal['documentIds']:
+                    document = get_document_metadata(base_url, cached_token, user_key, document_id)
+
+                    for entry in journal['ledgerEntries']:
+                        account = accounts[entry['accountId']]
+
+                        os.makedirs(f'{output}/{account["name"]} - {account["id"]}', exist_ok=True)
+                        print(f'Saving document {document_id} as {output}/{account["name"]} - {account["id"]}/{entry["date"]}_{entry["amount"]}_{journal["journalEntryText"]} - {journal["journalName"]} - {entry["text"]}.pdf')
+                        save_document_pdf(base_url, cached_token, user_key, document_id, f'{output}/{account["name"]} - {account["id"]}/{entry["date"]}_{entry["amount"]}_{journal["journalEntryText"]} - {journal["journalName"]} - {entry["text"]}.pdf')
+
+
+
+        else:
+            print('Failed to fetch API token. Exiting...')
 
